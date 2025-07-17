e3ada27e9dfa8d7e407fde8999a0f710ba2bfcfa
ayankhan434 <ayankhanayan992@gmail.com>
2025-07-17 23:12:29 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,15 @@
+a=100
+b=10
+print("the value of", a, " + ", 10,"is: ", a + b )
+print("the value of", a, " - ", 10,"is: ", a - b )
+print("the value of", a, " * ", 10,"is: ", a * b )
+print("the value of", a, " / ", 10,"is: ", a / b )
+
+
+
+
+
+
+
+
+
