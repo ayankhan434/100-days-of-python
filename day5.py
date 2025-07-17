25cd9998bbbb704874234a39074658953e7403b0
ayankhan434 <ayankhanayan992@gmail.com>
2025-07-17 21:40:22 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,7 @@
+# hey harry , how are you 
+""""
+hii, 
+"""
+print("hey i am  a good guy \nharry bhai also a good guy ")
+print("hey", 5,6, sep="~" ,end="ayan\n")
+print("harry bhai you are my idol")
